216c49a488749375c7000b01c2b1d5be5dcbf72a
yechenyan <ccyechenyan@gmail.com>
2024-12-16 02:39:50 +0800

    feat: update data

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: "/static",
   output: "export",
   basePath: "/pypsa-visual",
 };
